04e0f4060b8c42479590e8414a96ec69bc9b7d7e
Márcio Trovão <mc872684@gmail.com>
2022-08-31 13:45:58 -0300

    Atualização de arquivos

--- assets/config/config.js
+++ assets/config/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyD0nFD_T1sESAqXMPrjJWOe9qZC05njS_k",
-    authDomain: "projeto-mundo-invertido-ead44.firebaseapp.com",
-    projectId: "projeto-mundo-invertido-ead44",
-    storageBucket: "projeto-mundo-invertido-ead44.appspot.com",
-    messagingSenderId: "435623596630",
-    appId: "1:435623596630:web:60faabefb9bc8a6c20e1f7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
